6c817b860495ed79e5e08fa27c13c12eaf03f62d
Shaheer Raja <sraja7272@tamu.edu>
2021-04-10 15:29:35 -0500

    - Fully implemented Spotify API to get all songs by provided artist
    Signed-off-by: Shaheer Raja <sraja7272@tamu.edu>

--- Music Backend.py
+++ Music Backend.py
@@ -1,5 +1,5 @@
 import requests
-
+#*************************************AUTHORIZATION FOR SPOTIFY API*************************************#
 #Setting Application Details
 CLIENT_ID = '1b60decc28894577a21fbc138bfd5fd8'
 CLIENT_SECRET = 'aca051263ed0473bb4e83a7e08e5e990'
@@ -25,14 +25,16 @@ headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
+
+#*************************************Get all Artists with provided name*************************************#
 #Set Base URL of all Spotify API endpoints
 BASE_URL = 'https://api.spotify.com/v1/'
 
-#Artist name
-artist = 'Taylor Swift'
+#Inputted Artist name
+artistInput = 'Drake'
 
 #GET request with proper header for all artists with provided name
-result = requests.get(BASE_URL + 'search/', headers=headers, params={'q' : artist,'type' : 'artist'})
+result = requests.get(BASE_URL + 'search/', headers=headers, params={'q' : artistInput,'type' : 'artist'})
 result = result.json()
 
 #Parse result and get artist Name, ID, Genres, and Image
@@ -63,8 +65,13 @@ for i in result['artists']['items']:
 # print(artistImageArray)
 # print(artistGenreArray)
 
+
+#*************************************Get all Albums related to chosen Artist*************************************#
+chosenArtistName = artistNameArray[0]
+chosenArtistID = artistIDArray[0]
+
 #GET request with proper header to find all albums by chosen artist
-result = requests.get(BASE_URL + 'artists/' + artistIDArray[1] + '/albums', headers=headers)
+result = requests.get(BASE_URL + 'artists/' + chosenArtistID + '/albums', headers=headers)
 result = result.json()
 
 #Parse result and get album Name and ID
@@ -77,10 +84,12 @@ for i in result['items']:
 # print(albumNameArray)
 # print(albumIDArray)
 
+
+#*************************Get all Tracks related to all Albums related to chosen Artist*************************#
 #Convert Album ID Array to a string seperated by commas to be passed as a paramater below
 seperator = ','
 albumIDArrayToStr = seperator.join(albumIDArray)
-print(albumIDArrayToStr)
+# print(albumIDArrayToStr)
 
 #GET request with proper header to find all songs given albums
 result = requests.get(BASE_URL + 'albums/', headers=headers, params={'ids':albumIDArrayToStr})
@@ -94,5 +103,36 @@ for i in result["albums"]:
         trackNameArray.append(j["name"])
         trackIDArray.append(j["id"])
 
-print(trackNameArray)
-print(trackIDArray)
+# print(trackNameArray)
+# print(trackIDArray)
+
+
+#*************************Get Popularity score for each track*************************#
+#Convert track ID Array to a multiple TrackID string arrays seperated by commas to be passed as a paramater below
+seperator = ','
+combineTrackIDArray = []
+count = 0
+for i in range(0,len(trackIDArray),40):
+    tempTrackIDArray = trackIDArray[i:i+40]
+    combineTrackIDArray.append(seperator.join(tempTrackIDArray))
+
+# for i in combineTrackIDArray:
+#     print(i)
+
+#GET request with proper header to find all songs given albums
+trackResult = []
+for i in range(len(combineTrackIDArray)):
+    result = requests.get(BASE_URL + 'tracks/', headers=headers, params={'ids':combineTrackIDArray[i]})
+    trackResult.append(result.json())
+
+#Parse result and get popularity of each track
+trackPopularityArray = []
+for i in trackResult:
+    for j in i["tracks"]:
+        trackPopularityArray.append(j["popularity"])
+
+# print(trackPopularityArray)
+
+for i in range(len(trackNameArray)):
+    print(("Track Name: " + trackNameArray[i]).ljust(60),("\tPopularity Score: " + str(trackPopularityArray[i])).center(0),
+          ("\tArtist: " + chosenArtistName).rjust(0))
