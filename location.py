c6fb8d98b70b2acfc3da083a390def060c9cb5c5
Yasin Alam <yasin.alam@tamu.edu>
2021-04-10 22:58:20 -0500

    Add initialze location functionality

---
+++ location.py
@@ -0,0 +1,38 @@
+#Packages
+import numpy as np
+import pandas as pd
+import pgeocode as pg
+
+class Query:
+    #Data
+        def __init__(self, country = None, zipp = None): #Use shell = true to just use functions, else create object
+            if(country and zipp == None):
+                self.shell = True
+            else:
+                self.shell = False
+            self.country = country
+            self.zipp = zipp
+            self.region = None
+            self.info = None
+            if(not self.shell):
+                self.region = self.get_region(country)
+                self.info = self.get_info(zipp, country)
+                self.latitude = self.info.latitude
+                self.longitude = self.info.longitude
+
+        def get_region(self, country_ = None): #Get repo of country zips
+            if(self.shell):
+                return pg.Nominatim(country_)
+            else:
+                return pg.Nominatim(self.country)
+            
+        def get_info(self, zipp_ = None, country_ = None): # Get dataframe of info for a zip
+            if(not self.shell):
+                return self.region.query_postal_code(zipp_)
+            else:
+                reg = self.get_region(country_)
+                return reg.query_postal_code(zipp_)
+
+
+#Can use as object or just an engine
+        
